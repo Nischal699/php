257f386d92e541ce47027eb425f812759a9248ef
Nischal699 <nischal123321@gmail.com>
2025-03-20 20:35:18 +0545

    commited

---
+++ form/simpleform.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+
+<!--Just use GET instead of POST in above code to see the difference in URL.-->
+</body>
+</html>
